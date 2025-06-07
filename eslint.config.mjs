644ddf3fbf41aa5de28c081d5dea514d8f695a6d
Wayne (Chen ShengWen) <wenwen357951@gmail.com>
2025-06-07 21:27:41 +0800

    Refactor styles in MyWelcome.vue for consistency and readability; update Configure.mdx for improved formatting and structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,20 @@
 // @ts-check
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      ...eslintPluginPrettierRecommended.rules,
+    },
+  },
   {
     files: ['**/*.js', '**/*.ts', '**/*.vue'],
     rules: {
